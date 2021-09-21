2bc428940cf8a1abb720c060a59bd1f9c2776dae
komalrajbhar <komal.rajbhar@ratnamcollege.edu.in>
2021-09-21 17:23:34 +0530

    view-course

--- src/views/admin/view-course.js
+++ src/views/admin/view-course.js
@@ -2,9 +2,12 @@ import React,{useState,useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import axios from 'src/axios';
 import { useParams } from 'react-router-dom'
-import { AccessTime } from '@material-ui/icons';
+import { ArrowRightAlt } from '@material-ui/icons';
+import { GiSandsOfTime,GiWatch } from "react-icons/gi";
+import { FaGlobe } from 'react-icons/fa';
 import {
   Grid,
+  Button,
   Container,
   Typography
 } from '@material-ui/core';
@@ -12,46 +15,84 @@ const useStyles = makeStyles((theme) => ({
   root:{
     backgroundColor: theme.palette.background.dark,
     height: '100%',    
-    padding:theme.spacing(2)
+    width:'100%',
+  },
+  primarySection:{
+    alignItems:'center',
+    justifyContent:'center',
+    marginLeft:'10px',
+    textAlign:'left',
+    backgroundPosition: 'right',
+    background:"url(https://png.pngtree.com/png-vector/20200610/ourlarge/pngtree-work-from-home-with-girl-working-with-laptop-computer-for-business-png-image_2225853.jpg) no-repeat",
+    backgroundSize: '45% 80%',
   },
   table: {
-    minWidth: 650,
+    minWidth: 900,
   },
   title:{
     fontSize:`2.3em`,
-    marginTop:'0.5em',
-    marginBottom:'0.5em',
+    marginTop:'0.1em',
     color:`#6760fe`,
     fontWeight:'600',
   },
-  about:{
+  sub:{
     fontSize:`1.1em `,
     color:`#525252`,
     fontWeight:`400`,
-    marginTop:'0.5em',
-    marginBottom:'0.2em',
+    marginTop:'0em',
   },
-  description:{
-    fontSize:'1.3em',
+  subtitle:{
+    fontSize:`1.1em `,
+    color:`#525252`,
+    fontWeight:`400`,
+    marginTop:'1em',
+  },
+  desc:{
+    fontSize:'20px',
     color:'#141414',
     marginTop:'0.1em',
     marginBottom:'0.5em',
     fontWeight:`600`,
-  },
-  label:{
-    fontSize:`1.2em `,
-    color:`#292929`,
-    fontWeight:`400`,
-    marginTop:'1.5em',
-    marginBottom:'0.1em',
+    marginRight:'400px',
   },
   days:{
-    fontSize:`1.1em `,
-    color:`#141414`,
+    fontSize:`1em `,
+    color:`#525252`,
     fontWeight:`600`,
     display:`flex`,
     marginTop:'0.1em',
-  }
+    marginLeft:'10px',
+  },
+  enrollBtn:{
+    marginTop:80,
+    marginRight: theme.spacing(2),
+    padding: theme.spacing(1.5),
+    width:180
+  }, 
+  line:{
+    fontSize:`1.1em `,
+    color:`#525252`,
+    fontWeight:`400`,
+    borderRight:'1px solid #727376',
+    display:`flex`,
+    padding:'2px',
+    marginLeft: '20px',
+    paddingLeft:'40px',
+  },
+  last:{
+    fontSize:`1.1em `,
+    color:`#525252`,
+    fontWeight:`400`,
+    marginRight:'40px',
+    display:`flex`,
+    marginLeft: '40px',
+  },
+  span:{
+    marginTop:'10px',
+    display:'flex',
+    borderBottom:'1px solid #727376',
+    width:'300px',
+  },
 }));
 export default function viewCourse() {
   const classes = useStyles();
@@ -70,34 +111,47 @@ export default function viewCourse() {
   }
     return (
       <div className={classes.root}>
-        <Container maxWidth="lg">
-              <Grid container spacing={3}>
-                <Grid item xs={12}>
-                    <Typography className={`${classes.title}`} variant="h5">
-                     {course.course_name}
-                    </Typography>
-                    <Typography className={classes.about} variant="h4">
+          <div className={`section-5 ${classes.primarySection}`} style={{minHeight: 'calc(100vh - 65px)'}}>
+            <Typography className={`${classes.title}`} variant="h5">
+              {course.course_name}
+            </Typography>
+            <Typography className={classes.sub}>
+            Learn new skills with a flexible online course
+            </Typography>
+            <Button variant="contained" color="primary" size="large" className={classes.enrollBtn} target="_blank">
+                      Enroll now <ArrowRightAlt />
+            </Button> 
+            <span className={classes.span}/>
+            <Typography className={classes.subtitle} variant="h4">
                     About this course
-                    </Typography>
-                    <Typography className={`${classes.description}`}>
+            </Typography>
+            <Typography className={`${classes.desc}`}>
                       {course.description}
-                    </Typography>
-                    <Typography className={`${classes.label}`} variant="h5">
-                      Particular days for this course 
-                    </Typography>
+             </Typography>       
+          </div>
+            <Container maxWidth="940px">
+                <Grid container spacing={3}>
+                  <Grid item xs={3} className={`${classes.line}`}>
+                    <GiSandsOfTime size = '50px' />
                     <Typography className={`${classes.days}`}>
-                      | {course.days} |
-                    </Typography>
-                    <Typography className={`${classes.label}`} variant="h5">
-                      Timing of this course
-                    </Typography>             
+                      Days | <br /> 
+                    </Typography>  
+                  </Grid> 
+                  <Grid item xs={4} className={`${classes.line}`}>
+                    <GiWatch size = '50px' />
                     <Typography className={`${classes.days}`}>
-                     <AccessTime />{course?.start_time} - {course?.end_time}
-                    </Typography>
-                  </Grid>                  
+                      Timing | <br/>
+                      {course?.start_time} - {course?.end_time}
+                    </Typography>  
+                  </Grid>                 
+                  <Grid item xs={3} className={`${classes.last}`}>
+                    <FaGlobe size = '50px' />
+                    <Typography className={`${classes.days}`}>
+                     100% online: <br/> Learn at your own place 
+                    </Typography>  
+                  </Grid>          
               </Grid>  
             </Container>          
         </div>  
-          
     );
 }
