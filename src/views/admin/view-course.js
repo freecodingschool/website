0ddd98e043b638818e36d4ae4513f4272103be6b
komalrajbhar <komal.rajbhar@ratnamcollege.edu.in>
2021-10-16 23:00:44 +0530

    view-course

--- src/views/admin/view-course.js
+++ src/views/admin/view-course.js
@@ -3,12 +3,13 @@ import { makeStyles } from '@material-ui/core/styles';
 import axios from 'src/axios';
 import { ArrowRightAlt } from '@material-ui/icons';
 import { useParams } from 'react-router-dom'
-import { AccessTime } from '@material-ui/icons';
 import {
   Grid,
+  Button,
   Typography,
   Container
 } from '@material-ui/core';
+import Courses from './course';
 const useStyles = makeStyles((theme) => ({
   root:{
     backgroundColor: theme.palette.background.dark,
@@ -40,27 +41,10 @@ course_image: {
 },
 
 course_schedule :{
-    margin: '0px 0px 0px 10px',
+    marginLeft: '20px',
     fontWeight: '700',
 
 },
-
-course_enroll_btn: {
-    backgroundColor: '#5d5fef',
-    border: 'none',
-    borderRadius: '3px',
-    color: 'white',
-    //padding: '10px',
-    textAlign: 'center',
-    textDecoration: 'none',
-    display: 'inline-block',
-    fontSize: '20px',
-    //margin: '20px 10px 0px 10px'
-    marginLeft:'450px',
-    padding : theme.spacing(1.5),
-    width:'180',
-},
-
 about_course: {
     fontSize: '24px',
     textTransform: 'uppercase',
@@ -91,30 +75,14 @@ about_course: {
       marginTop: '10px',
       textTransform: 'none',
   },
-  title:{
-    marginTop:'0.5em',
-    marginBottom:'0.5em',
+  course_background:{
+    background:'#C6C7F6',
   },
-  about:{
-    fontWeight:`400`,
-    marginTop:'0.5em',
-    marginBottom:'0.2em',
-  },
-  description:{
-    marginTop:'0.1em',
-    marginBottom:'0.5em',
-  },
-  label:{
-    marginTop:'1.5em',
-    marginBottom:'0.1em',
-  },
-  days:{
-    display:`flex`,
-    marginTop:'0.1em',
-    '& icon': {
-      fontSize:14
-    }
-  }
+  enrollBtn:{
+    margin:'3em 6em',
+    width:180,
+    float:'right', 
+ },
 }));
 export default function viewCourse() {
   const classes = useStyles();
@@ -136,29 +104,92 @@ export default function viewCourse() {
       <div className={classes.root}>
         <Container maxWidth="lg">
               <Grid container spacing={3}>
-                <Grid item xs={12}>
-                    <Typography className={`${classes.title}`} variant="h5">
+                <Grid item md={12} className={`${classes.course_background}`}>
+                    <Typography className={`${classes.course_name}`} variant="h5">
                      {course.course_name}
                     </Typography>
-                    <Typography className={classes.about} variant="subtitle1">
-                    About this course
+                    <Grid container spacing={3}>
+                    <Grid item md={7}>
+                    <Typography className={classes.course_description} variant="subtitle1">
+                    {course.description}
                     </Typography>
-                    <Typography className={`${classes.description}`}>
-                      {course.description}
+                    </Grid>
+                    <Grid item md={5}>
+                      <img width="300px">{course.course_image}</img>
+                    </Grid>
+                    </Grid>
+                    <Grid container spacing={3}>
+                    <Grid container md={6}>
+                    <Grid item md={12}>
+                    <Typography className={`${classes.course_schedule}`} variant="subtitle1">
+                      Instructor : {course.instructor} 
                     </Typography>
-                    <Typography className={`${classes.label}`} variant="subtitle1">
-                      Date 
-                    </Typography>
-                    <Typography className={`${classes.days}`}>
-                      | {course.days} |
-                    </Typography>
-                    <Typography className={`${classes.label}`} variant="subtitle1">
-                      Timing
+                    </Grid>
+                    <Grid item md={12}>
+                    <Typography className={`${classes.course_schedule}`} variant="subtitle1">
+                      Timing : {course?.start_time} - {course?.end_time}
                     </Typography>             
-                    <Typography className={`${classes.days}`}>
-                     <AccessTime />{course?.start_time} - {course?.end_time}
+                    </Grid> 
+                    <Grid item md={12}>
+                    <Typography className={`${classes.course_schedule}`} variant="subtitle1">
+                      Platform : ZOOM 
+                    </Typography>             
+                    </Grid></Grid>  
+                    <Grid container md={6}>
+                    <Grid item md={12}>
+                    <Button variant="contained" color="primary" size="large" className={`${classes.enrollBtn}`} href="https://forms.gle/1CSaz5JA4zJe2UHu9" target="_blank">
+                        Enroll now <ArrowRightAlt />
+                    </Button>              
+                    </Grid>
+                    </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item md={12}>                  
+                  <Typography className={classes.about_course}>
+                    About
+                  </Typography>
+                </Grid>
+                <Grid item md={12}>
+                  <Typography className={`${classes.heading}`}>
+                    WHAT YOU'LL LEARN?                  
+                  </Typography>
+                    <Typography className={`${classes.point1}`}>
+                      {course.learning}
                     </Typography>
-                  </Grid>                  
+                </Grid>
+                <Grid item md={12}>
+                    <Typography className={`${classes.heading}`} variant="subtitle1">
+                      WHO THIS COURSE IS FOR? 
+                    </Typography>
+                    <Typography className={`${classes.point2}`}>
+                       {course.about} 
+                    </Typography>
+                </Grid>
+                <Grid item md={12}>
+                    <Typography className={`${classes.heading}`} variant="subtitle1">
+                      COURSE CONTENT
+                    </Typography>             
+                    <Typography className={`${classes.point1}`}>
+                      {course.content}
+                    </Typography>
+                </Grid>     
+                <Grid item md={12}>
+                    <Typography className={`${classes.heading}`} variant="subtitle1">
+                      REQUIREMENTS  
+                    </Typography>             
+                    <Typography className={`${classes.point2}`}>
+                      {course.requirements}
+                    </Typography>
+                </Grid>               
+                <Grid item md={12}>
+                    <Typography className={`${classes.heading}`} variant="subtitle1">
+                      READINGS 
+                    </Typography>             
+                    <Typography className={`${classes.point1}`}>
+                      {course.readings}
+                    </Typography>
+                </Grid>               
+                        
               </Grid>  
             </Container>          
         </div> 
